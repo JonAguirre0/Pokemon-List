71e185947b508658e98d4e0f9361c02b07274e48
Jonathan Aguirre <jaguirrecsu@gmail.com>
2025-10-26 16:38:17 -0700

    setup for api gets

--- server.js
+++ server.js
@@ -20,3 +20,37 @@ async function fetchAndCache(key, url) {
     cardCache.set(key, data)
     return data
 }
+
+const API_LINK = 'https://api.tcgdex.net/v2/en'
+
+app.get('/selectedSet', async (req, res) => {
+    const choice = req.query.choice
+    const cacheKey = `${choice}_set`
+
+    const url = `${API_LINK}/sets/${choice}`
+    const data = await fetchAndCache(cacheKey, url)
+    res.json(data)
+})
+
+app.get('/sets', async (req, res) => {
+    const cacheKey = `sets`
+    const url = `${API_LINK}/sets`
+    const data = await fetchAndCache(cacheKey, url)
+    res.json(data)
+})
+
+app.get('/selectedSeries', async (req, res) => {
+    const choice1 = req.query.choice1
+    const cacheKey = `${choice1}_series`
+
+    const url = `${API_LINK}/series/${choice1}`
+    const data = await fetchAndCache(cacheKey, url)
+    res.json(data)
+})
+
+app.get('/series', async (req, res) => {
+    const cacheKey = `series`
+    const url = `${API_LINK}/series`
+    const data = await fetchAndCache(cacheKey, url)
+    res.json(data)
+})
